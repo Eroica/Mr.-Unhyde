b155b47addf8ba49b0f561d31c616c3d09fec797
Eroica <Eroica@users.noreply.github.com>
2024-04-01 13:20:06 +0200

    Set up and remove local listener during PreferencesWindow lifecycle

--- Mr. Unhyde/PreferencesController.m
+++ Mr. Unhyde/PreferencesController.m
@@ -17,7 +17,7 @@
 
 - (instancetype)init {
     self = [super initWithWindowNibName:@"Preferences"];
-    
+
     return self;
 }
 
@@ -27,12 +27,26 @@
 
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     self.window.delegate = self;
-    self.localMouseHandler = [self.mouseObserver setupLocalEventHandler];
 }
 
 
 - (IBAction)onPreferencesChanged:(id)sender {
     [self.mouseObserver updateWithUserDefaults];
+}
+
+
+- (IBAction)showWindow:(id)sender
+{
+    [super showWindow:sender];
+    if (self.localMouseHandler == nil) {
+        self.localMouseHandler = [self.mouseObserver setupLocalEventHandler];
+    }
+}
+
+
+- (void)windowWillClose:(NSNotification *)notification {
+    [NSEvent removeMonitor:self.localMouseHandler];
+    self.localMouseHandler = nil;
 }
 
 @end
